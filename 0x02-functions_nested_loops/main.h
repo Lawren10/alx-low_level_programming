c323ff095289568b1c48d839fdac0257d2261dc4
Lawrence Ogereka <lawrenceanthony70@gmail.com>
2022-06-07 06:19:31 -0700

    updated main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * this file is a header file that defines functions prototype for use
+ *
+ * Description: defines functions prototypes
+ *
+ * Return: 0 (Success)
+ */
+
+void _putchar(char letter)
+{
+
+	putchar(letter)
+	return
+
+}
