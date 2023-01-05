8f72f2a7976d317148ecce1805a06cc02ea17076
Lawrence Ogereka <lawrenceanthony70@gmail.com>
2023-01-05 11:32:40 -0800

    search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,23 +1,31 @@
 #include "search_algos.h"
+
 /**
-  * linear_search - searches for a value in an array of integers using linear
-  * search.
-  * @array: pointer to first element of array to search
-  * @size: number of elements in array
-  * @value: value to search for
-  * Return: first index where value is located or -1
-  */
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t idx = 0;
+	size_t x;
 
-	while (idx < size)
+	if (array == NULL)
 	{
-		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
-		if (array[idx] == value)
-			return (idx);
-		idx++;
+		return (-1);
 	}
 
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,45 @@
 #include "search_algos.h"
+
 /**
-  * binary_search - searches for a value in an array of integers using binary
-  * search.
-  * @array: pointer to first element of array to search
-  * @size: number of elements in array
-  * @value: value to search for
-  * Return: first index where value is located or -1
-  */
+ * binary_search - searches for a value in an integer array using a binary
+ * search algorithm, not guaranteed to return lowest index if `value` appears
+ * twice in `array`
+ * @array: pointer to first element of array to seach
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, last_idx, idx = 0;
-	int m;
+	int low, mid, high;
+	int x;
 
 	if (array == NULL)
-		return (-1);
-
-	last_idx = size - 1;
-
-	while (idx <= last_idx)
 	{
-		printf("Searching in array: ");
-
-		for (i = idx; i <= last_idx; i++)
-		{
-			printf("%d", array[i]);
-			if (i != last_idx)
-				printf(", ");
-			else
-				printf("\n");
-		}
-		m = (idx + last_idx) / 2;
-
-		if (array[m] < value)
-			idx = m + 1;
-		else if (array[m] > value)
-			last_idx = m - 1;
-		else
-			return (m);
+		return (-1);
 	}
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (x = low; x <= high; x++)
+			printf("%i%s", array[x], x == high ? "\n" : ", ");
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
+
 	return (-1);
 }
